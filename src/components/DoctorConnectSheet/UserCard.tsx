a90d7d6a593245f867c640b6e57c267c8e111722
Khavin Shankar <khavinshankar@gmail.com>
2025-04-21 17:29:37 +0530

    added whatsapp conversation starter message

--- src/components/DoctorConnectSheet/UserCard.tsx
+++ src/components/DoctorConnectSheet/UserCard.tsx
@@ -11,15 +11,84 @@ import { formatDate, formatUserName, toast } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 import { FacilityUser } from "@/types/user";
+import { I18NNAMESPACE } from "@/lib/constants";
 import { formatPhoneNumberIntl } from "react-phone-number-input";
+import { useTranslation } from "react-i18next";
 
 type UserCardProps = {
   user: FacilityUser;
 };
 
+type MSLaunchURI = (
+  uri: string,
+  successCB?: null | (() => void),
+  noHandlerCB?: null | (() => void)
+) => void;
+
 export default function UserCard({ user: facilityUser }: UserCardProps) {
+  const { t } = useTranslation(I18NNAMESPACE);
+
   const user = facilityUser.user;
   const userFullName = formatUserName(user);
+
+  function connectOnWhatsApp(e: React.MouseEvent<HTMLButtonElement>) {
+    e.stopPropagation();
+    if (!user.phone_number) return;
+    const phoneNumber = user.phone_number?.replace(/\D+/g, "");
+    const message = t("whatsapp_chat_start_message", {
+      userName: userFullName,
+      encounterLink: window.location.href,
+    });
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappAppURL = `whatsapp://send?phone=${phoneNumber}&text=${encodedMessage}`;
+    const whatsappWebURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    const userAgent = navigator.userAgent;
+    const isEdge = /edge\/\d+/i.test(userAgent);
+    const isMobileFirefoxOrSafari =
+      /iPhone|iPad|iPod|Android/i.test(userAgent) &&
+      (/Firefox/i.test(userAgent) ||
+        (/Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)));
+    const isSafari = /Safari/i.test(userAgent) && !/Chrome/i.test(userAgent);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
+
+    const openWhatsAppWebFallback = () => {
+      if (isMobile) {
+        toast.warning(t("whatsapp_not_installed"));
+      }
+      window.open(whatsappWebURL, "_blank");
+    };
+
+    if (isEdge) {
+      if ("msLaunchUri" in navigator) {
+        const launch = navigator.msLaunchUri as MSLaunchURI;
+        launch(whatsappAppURL, null, openWhatsAppWebFallback);
+      } else {
+        openWhatsAppWebFallback();
+      }
+    } else {
+      const attemptOpenWhatsApp = (url: string) => {
+        if (isMobileFirefoxOrSafari || isSafari) {
+          const iframe = document.createElement("iframe");
+          iframe.style.display = "none";
+          iframe.src = url;
+          document.body.appendChild(iframe);
+        } else {
+          window.location.href = url;
+        }
+      };
+
+      attemptOpenWhatsApp(whatsappAppURL);
+
+      const fallbackTimeout = setTimeout(() => {
+        openWhatsAppWebFallback();
+      }, 1250);
+
+      window.addEventListener("blur", () => {
+        clearTimeout(fallbackTimeout);
+      });
+    }
+  }
 
   return (
     <Card className="overflow-hidden">
@@ -68,9 +137,7 @@ export default function UserCard({ user: facilityUser }: UserCardProps) {
                 variant="ghost"
                 size="icon"
                 title="WhatsApp"
-                onClick={() => {
-                  window.open(`https://wa.me/${user.phone_number}`);
-                }}
+                onClick={connectOnWhatsApp}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
@@ -82,7 +149,7 @@ export default function UserCard({ user: facilityUser }: UserCardProps) {
               variant="ghost"
               onClick={() => {
                 navigator.clipboard.writeText(user.phone_number);
-                toast.success("Phone number copied to clipboard");
+                toast.success(t("phone_number_copied"));
               }}
               className="text-xs text-muted-foreground whitespace-nowrap flex gap-1 items-center"
             >
